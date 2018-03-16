97329b4ca7ea90f81c2a1bccec5600c90fc380d8
markforest <toxerius7@gmail.com>
2018-03-16 20:35:18 +0200

    class DataTable

---
+++ AdoNetLesson3/AdoNetLesson3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AdoNetLesson3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnExacute = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(456, 278);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnExacute
+            // 
+            this.btnExacute.Location = new System.Drawing.Point(371, 9);
+            this.btnExacute.Name = "btnExacute";
+            this.btnExacute.Size = new System.Drawing.Size(75, 23);
+            this.btnExacute.TabIndex = 2;
+            this.btnExacute.Text = "Exacute";
+            this.btnExacute.UseVisualStyleBackColor = true;
+            this.btnExacute.Click += new System.EventHandler(this.btnExacute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 345);
+            this.Controls.Add(this.btnExacute);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnExacute;
+    }
+}
+
---
+++ AdoNetLesson3/AdoNetLesson3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdoNetLesson3
+{
+    public partial class Form1 : Form
+    {
+        SqlDataReader reader;
+        DataTable dataTable;
+        SqlConnection sqlConnection = null;
+        string connString = @"Data Source=CR4-00\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;";
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void btnExacute_Click(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(connString);
+
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = textBox1.Text;
+                sqlCommand.Connection = sqlConnection;
+
+                dataGridView1.DataSource = null;
+
+                sqlConnection.Open();
+                dataTable = new DataTable();
+                reader = sqlCommand.ExecuteReader();
+                int line = 0;
+                while (reader.Read())
+                {
+                    if(line == 0)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            dataTable.Columns.Add(reader.GetName(i));
+                        }
+                    }
+                    line++;
+                    DataRow row = dataTable.NewRow();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row[i] = reader[i];
+                    }
+                    dataTable.Rows.Add(row);
+                }
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                {
+                    sqlConnection.Close();
+                }
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
